01e9b5746887701cb95821aa12a416d3711c4e7e
ErikaCetera <ceteraerika@gmail.com>
2025-03-20 15:52:56 +0100

    feat: incapsulamento

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,21 +2,22 @@ package org.lessons.java.shop;
 
 import java.math.BigDecimal;
 
-
 public class Main {
-public static void main(String[] args) {
+    public static void main(String[] args) {
+
+        Product product1 = new Product("Product Name", "Product Description", new BigDecimal("19.99"),
+                new BigDecimal("0.22"));
+
+        Product product2 = new Product("Product Name2", new BigDecimal("19.99"));
+
+        System.out.println(product1.getBasicPrice());
+        System.out.println(product1.getPriceVat());
+        System.out.println(product1.getCodeName());
+        System.out.println(product1.getCode());
 
 
-    Product product1 = new Product("Product Name", "Product Description", new BigDecimal("19.99"), new BigDecimal("0.22"));
-   
-   System.out.println(product1.code);
-   System.out.println(product1.getBasicPrice());
-   System.out.println(product1.getPriceVat());
-   System.out.println(product1.getCodeName());
-   
+        System.out.println(product2.getProduct());
+        
+    }
 
 }
-
-      
-}
-
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -7,22 +7,83 @@ import java.util.Random;
 public class Product {
 
     // attributi
-    public int code;
-    public String name;
-    public String description;
-    public BigDecimal price;
-    public BigDecimal vat;
+    private int code;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private BigDecimal vat;
+
 
     // capicità
+
+    //costruttore 1
     public Product(String name, String description, BigDecimal price, BigDecimal vat) {
         Random r = new Random();
-        int max = 10;
+        int max = 1000;
         this.code = r.nextInt(max);
         this.name = name;
         this.description = description;
         this.price = price;
         this.vat = vat;
     }
+
+    //costruttore 2
+    public Product( String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+        
+    }
+
+   // metodi getter e setter
+
+   //code
+    public int getCode(){
+        return this.code;
+    }
+
+    
+    //name
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName( String name){
+        this.name = name;
+    }
+
+    //description
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription( String description){
+        this.description = description;
+    }
+
+    //price
+    public BigDecimal getPrice(){
+        return this.price;
+    }
+
+    public void setPrice( BigDecimal price){
+        this.price = price;
+    }
+
+    //vat
+    public BigDecimal getVat(){
+        return this.vat;
+    }
+
+    public void setVat( BigDecimal vat){
+        this.vat = vat;
+    }
+
+
+    // altri metodi
+   public String getProduct(){
+    return name + " " + price;
+   }
+
 
     public BigDecimal getBasicPrice() {
         return this.price;
